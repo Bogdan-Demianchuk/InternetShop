00d36746434d91e99f04d6b7798dc46089dfe724
Богдан Демьянчук <t0974447144@gmail.com>
2020-04-23 16:47:52 +0300

    temp commit

---
+++ src/main/java/mate/academy/internetshop/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,43 @@
+package mate.academy.internetshop.dao.impl;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+import mate.academy.internetshop.dao.OrderDao;
+import mate.academy.internetshop.dao.Storage;
+import mate.academy.internetshop.lib.Dao;
+import mate.academy.internetshop.model.Order;
+
+@Dao
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public Order create(Order order) {
+        Storage.addOrder(order);
+        return order;
+    }
+
+    @Override
+    public Optional<Order> get(Long orderId) {
+        return Storage.orders.stream()
+                .filter(i -> i.getOrderId().equals(orderId))
+                .findFirst();
+    }
+
+    @Override
+    public List<Order> getAll() {
+        return Storage.orders;
+    }
+
+    @Override
+    public Order update(Order order) {
+        IntStream.range(0, Storage.orders.size())
+                .filter(x -> order.getOrderId().equals(Storage.shoppingCarts.get(x).getShoppingCartId()))
+                .forEach(i -> Storage.orders.set(i, order));
+        return order;
+    }
+
+    @Override
+    public boolean delete(Long orderId) {
+        return Storage.orders.removeIf(i -> i.getOrderId().equals(orderId));
+    }
+}
---
+++ src/main/java/mate/academy/internetshop/dao/impl/UserDaoImpl.java
@@ -0,0 +1,44 @@
+package mate.academy.internetshop.dao.impl;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+import mate.academy.internetshop.dao.Storage;
+import mate.academy.internetshop.dao.UserDao;
+import mate.academy.internetshop.lib.Dao;
+import mate.academy.internetshop.model.User;
+
+@Dao
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User create(User user) {
+        Storage.addUser(user);
+        return user;
+    }
+
+    @Override
+    public Optional<User> get(Long userId) {
+        return Storage.users.stream()
+                .filter(i -> i.getUserId().equals(userId))
+                .findFirst();
+    }
+
+    @Override
+    public List<User> getAll() {
+        return Storage.users;
+    }
+
+    @Override
+    public User update(User user) {
+        IntStream.range(0, Storage.users.size())
+                .filter(x -> user.getUserId().equals(Storage.users.get(x).getUserId()))
+                .forEach(i -> Storage.users.set(i, user));
+        return user;
+    }
+
+    @Override
+    public boolean delete(Long userId) {
+        return Storage.users
+                .removeIf(i -> i.getUserId().equals(userId));
+    }
+}
