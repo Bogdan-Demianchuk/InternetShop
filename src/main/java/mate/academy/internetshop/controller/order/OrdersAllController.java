f490be9a15752b74ec74b14e14acd499224d3d8a
Богдан Демьянчук <t0974447144@gmail.com>
2020-05-04 16:15:34 +0300

    style

--- src/main/java/mate/academy/internetshop/controller/order/OrdersAllController.java
+++ src/main/java/mate/academy/internetshop/controller/order/OrdersAllController.java
@@ -12,12 +12,13 @@ import mate.academy.internetshop.service.OrderService;
 import mate.academy.internetshop.service.UserService;
 
 public class OrdersAllController extends HttpServlet {
+    private static final String USER_ID = "user_id";
     private static final Injector INJECTOR = Injector.getInstance("mate.academy");
     private OrderService orderService =
             (OrderService) INJECTOR.getInstance(OrderService.class);
     private UserService userService =
             (UserService) INJECTOR.getInstance(UserService.class);
-    private static final String USER_ID = "user_id";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
